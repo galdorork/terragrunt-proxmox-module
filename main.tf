65bd82c00647fdd8dc7283a9827a13bef8961209
galdor <victor.oliveira.comp@gmail.com>
2020-10-13 16:35:59 +0200

    Variable name vm_name updated

--- main.tf
+++ main.tf
@@ -21,24 +21,24 @@ resource "proxmox_vm_qemu" "proxmox_vm" {
   memory            = var.memory
   scsihw            = var.diskhw
   bootdisk          = var.bootdisk
-disk {
+
     id              = 0
     size            = var.disk_size
     type            = var.disk_type
     storage         = var.lvm_storage
     storage_type    = var.storage_type
     iothread        = var.iothread
-  }
-network {
+  
+
     id              = var.network_id
     model           = var.network_model
     bridge          = var.network_bridge
-  }
-lifecycle {
+  
+
     ignore_changes  = [
       network,
     ]
-  }
+  
 # Cloud Init Settings (Change the IP range and the GW to suit your needs)
   ipconfig0 = "ip=${var.ip}/24,gw=10.10.10.1"
 sshkeys = <<EOF
