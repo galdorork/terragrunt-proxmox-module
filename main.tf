49e130bdbf7e66eb484a1fa54a1c07a2520351cd
galdor <victor.oliveira.comp@gmail.com>
2020-10-13 15:37:12 +0200

    initial module commit

---
+++ main.tf
@@ -0,0 +1,47 @@
+# Change $PROXMOXSERVERIP to one of your Proxmox Node's IPs or FQDN.
+# Change $SUPERSECRETPASSWORD to the root password of the node.
+provider "proxmox" {
+    pm_api_url = "https://${var.node_ip}:8006/api2/json"
+    pm_user = "root@pam"
+    pm_password = var.node_password
+    pm_tls_insecure = "true"
+}
+
+# Change $NODETOBEDEPLOYED to the nmode where you want the VMs to be created at.
+# Change $LVMSTORAGENAME to the storage that the VM's disk is going to be created at.
+resource "proxmox_vm_qemu" "proxmox_vm" {
+  count             = var.vm_count
+  name              = var.vm_name[count.index]
+  target_node       = var.node
+  clone             = var.template
+  os_type           = var.os_type
+  cores             = var.cores
+  sockets           = var.sockets
+  cpu               = var.cpuhw
+  memory            = var.memory
+  scsihw            = var.diskhw
+  bootdisk          = var.bootdisk
+disk {
+    id              = 0
+    size            = var.disk_size
+    type            = var.disk_type
+    storage         = var.lvm_storage
+    storage_type    = var.storage_type
+    iothread        = var.iothread
+  }
+network {
+    id              = var.network_id
+    model           = var.network_model
+    bridge          = var.network_bridge
+  }
+lifecycle {
+    ignore_changes  = [
+      network,
+    ]
+  }
+# Cloud Init Settings (Change the IP range and the GW to suit your needs)
+  ipconfig0 = "ip=${var.ip}/24,gw=10.10.10.1"
+sshkeys = <<EOF
+  ${var.ssh_key}
+  EOF
+}
---
+++ variables.tf
@@ -0,0 +1,105 @@
+  variable "vm_count" {
+      description = "(Required) Amount of VMs to be created"
+      default = "0"
+      type = "string"
+  }
+  variable "vm_name[count.index]" {
+      description = "(Required) VM Name"
+      default = "tf"
+      type = "string"
+  }
+  variable "node" {
+      description = "(Required) Proxmox Node IP to be used"
+      default = ""
+      type = "string"
+  }
+  variable "template" {
+      description = "(Required) Source template to be used, this example uses debian-cloudinit."
+      default = "debian-cloudinit"
+      type = "string"
+  }
+  variable "os_type" {
+      description = "(Optional) Type of VM to be created, this example needs cloud-init"
+      default = "cloud-init"
+      type = "string"
+  }
+  variable "cores" {
+      description = "(Required) Amount of CPU Cores to be used by the VM"
+      default = "1"
+      type = "string"
+  }
+  variable "sockets" {
+      description = "(Optional) Amount of CPU Sockets to be used by the VM, by default it assumes one socket"
+      default = "1"
+      type = "string"
+  }
+  variable "cpuhw" {
+      description = "(Optional) CPU Arquitecture to be used by the VM, by default it uses the host CPU architecture definition"
+      default = "host"
+      type = "string"
+  }
+  variable "memory" {
+      description = "(Required) VM memory amount in MiB"
+      default = "1024"
+      type = "string"
+  }
+  variable "diskhw" {
+      description = "(Optional) Disk driver to be used, by default it uses virtio-scsi-pci"
+      default = "virtio-scsi-pci"
+      type = "string"
+  }
+  variable "bootdisk" {
+      description = "(Optional) Disk to boot from, by default it uses the first disk created"
+      default = "scsi0"
+      type = "string"
+  }
+  variable "disk_size" {
+      description = "Optional) Disk dize for the VM in GiB, by default it's set to 20GiB"
+      default = "20"
+      type = "string"
+  }
+  variable "disk_type" {
+      description = "(Optional) Disk type definition for Proxmox"
+      default = "scsi"
+      type = "string"
+  }
+  variable "lvm_storage" {
+      description = "(Required) Storage lvm volume where the disk will be created"
+      default = ""
+      type = "string"
+  }
+  variable "storage_type" {
+      description = "(Optional) Volume type for the volume where the disk will be created"
+      default = "lvm"
+      type = "string"
+  }
+  variable "iothread" {
+      description = "(Optional) iothread Enabled by default, improves disk performance on LVM"
+      default = "true"
+      type = "string"
+  }
+  variable "network_id" {
+      description = "(Optional) Virtual Network card ID to be used, defaults to 0"
+      default = "0"
+      type = "string"
+  }
+  variable "network_model" {
+      description = "(Optional) Virtual Network card model to be used, defaults to virtio"
+      default = "virtio"
+      type = "string"
+  }
+  variable "network_bridge" {
+      description = "(Optional) Virtual Network bridge to be used by the VM, uses vmbr0 as default bridge created by Proxmox"
+      default = "vmbr0"
+      type = "string"
+  }
+  variable "ip" {
+      description = "(Required) IP to be used by the VM"
+      default = ""
+      type = "string"
+  }
+  variable "ssh_key" {
+      description = "(Required) SSH Key to be configured for access to the VM, you won't be able to access the VMs if this value isn't set up"
+      default = ""
+      type = "string"
+  }
